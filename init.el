f71761cc8ed4b2786cf60e7a78abb236bde128f0
Edward Tsech <edtsech@gmail.com>
2013-05-16 07:27:40 +0200

    Remove multiple cursors

--- init.el
+++ init.el
@@ -32,15 +32,6 @@
 
 ;; PLUGINS
 
-;; multiple cursors
-(add-to-list 'load-path "~/.emacs.d/multiple-cursors/")
-(require 'multiple-cursors)
-
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
 ;; Evil-mode
 (require 'evil)
 (require 'evil-paredit)
