6494108525447b54d5c3258c4eadf6c0e8b0ec92
Edward Tsech <edtsech@gmail.com>
2013-05-16 07:51:35 +0200

    Simplify config.

--- init.el
+++ init.el
@@ -12,11 +12,6 @@
  '(tool-bar-mode nil)
  '(scroll-bar-mode nil))
 
-;(global-linum-mode t)
-
-
-(setq haskell-program-name "cabal-dev ghci")
-
 ;; Fix the PATH variable
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
@@ -32,19 +27,20 @@
 
 ;; PLUGINS
 
-;; Evil-mode
-(require 'evil)
-(require 'evil-paredit)
-;(add-hook 'evil-insert-state-entry-hook 'linum-relative-toggle)
-;(add-hook 'evil-insert-state-exit-hook 'linum-relative-toggle)
-(evil-mode 1)
-(setq evil-default-cursor t)
+;; Line numbers
+;; (global-linum-mode t)
 
-;; paredit
+;; Vim-mode (Evil-mode)
+;; (require 'evil)
+;; (require 'evil-paredit)
+;; (evil-mode 1)
+;; (setq evil-default-cursor t)
+
+;; Paredit
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 
-;; highlight-parentheses
+;; Highlight-parentheses
 (require 'highlight-parentheses)
 (define-globalized-minor-mode global-highlight-parentheses-mode
   highlight-parentheses-mode
@@ -52,21 +48,14 @@
     (highlight-parentheses-mode t)))
 
 (custom-set-variables '(hl-paren-colors (quote ("orange" "yellow" "greenyellow" "green" "springgreen" "cyan" "slateblue" "magenta" "purple"))))
-;(custom-set-variables '(hl-paren-colors (reverse (quote ("greenyellow" "green" "springgreen" "cyan")))))
-;(custom-set-variables '(hl-paren-colors (quote ("cyan"))))
-;(custom-set-variables '(hl-paren-colors (reverse (quote ("#B7E6FD" "#9EDEFD" "#7DD2FB" "#297BA3")))))
-
 (add-hook 'clojure-mode-hook 'highlight-parentheses-mode)
 
-;; midje-mode
-(require 'midje-mode)
-(add-hook 'clojure-mode-hook 'midje-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
+;; Midje-mode
+;; (require 'midje-mode)
+;; (add-hook 'clojure-mode-hook 'midje-mode)
+;; (add-hook 'clojure-mode-hook 'paredit-mode)
 
-;; rainbow delimiters
-;; (global-rainbow-delimiters-mode)
-
-;; nrepl
+;; Nrepl
 (setenv "PATH" (concat (getenv "HOME") "/bin:" (getenv "PATH")))
 (setq exec-path (cons "~/bin" exec-path))
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
@@ -79,28 +68,26 @@
 ;; (ac-config-default)
 
 ;; Super Tab
-(require 'smart-tab)
-(global-smart-tab-mode 1)
+;; (require 'smart-tab)
+;; (global-smart-tab-mode 1)
 
-;; Linum relative
-(require 'linum-relative)
-(set-face-foreground 'linum-relative-current-face nil)
-(set-face-background 'linum-relative-current-face nil)
+;; Relative line numbers
+;; (require 'linum-relative)
+;; (set-face-foreground 'linum-relative-current-face nil)
+;; (set-face-background 'linum-relative-current-face nil)
 
 ;; APPEARENCE
 
 ;; Set color-theme
 (require 'color-theme)
-(color-theme-initialize)
 (eval-after-load "color-theme"
   '(progn
-     ;(color-theme-initialize)
-     ;(color-theme-charcoal-black)
-     ;(color-theme-sanityinc-tomorrow-day)
+     (color-theme-initialize)
+     ;; (color-theme-sanityinc-tomorrow-day)
      (color-theme-sanityinc-tomorrow-eighties)
      ))
 
-(set-face-attribute 'default nil :height 130)
+(set-face-attribute 'default nil :height 120)
 (set-cursor-color 'white)
 
 (setq-default indent-tabs-mode nil)
@@ -108,23 +95,9 @@
 (setq-default truncate-lines t)
 
 ;; WHITESPACES
-(require 'whitespace)
-(add-hook 'after-save-hook 'whitespace-cleanup)
-(setq whitespace-line-column 90)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
-(global-whitespace-mode t)
-
-;; Set Speclj indentaion
-(put 'describe 'clojure-backtracking-indent '(4 2))
-(put 'it 'clojure-backtracking-indent '(4 2))
-(put 'before 'clojure-backtracking-indent '(2))
-(put 'before-all 'clojure-backtracking-indent '(2))
-(put 'after-all 'clojure-backtracking-indent '(2))
-(put 'after 'clojure-backtracking-indent '(2))
-
-;; Set Compojure indentation
-(put 'GET 'clojure-backtracking-indent '(4 4 2))
-(put 'POST 'clojure-backtracking-indent '(4 4 2))
-(put 'PUT 'clojure-backtracking-indent '(4 4 2))
-(put 'DELETE 'clojure-backtracking-indent '(4 4 2))
-(put 'PATCH 'clojure-backtracking-indent '(4 4 2))
+;; (require 'whitespace)
+;; (add-hook 'after-save-hook 'whitespace-cleanup)
+;; (setq whitespace-line-column 90)
+;; highlight trainling spaces, empty lines and etc
+;; (setq whitespace-style '(face empty tabs lines-tail trailing))
+;; (global-whitespace-mode t)
